fc01dd83986830c64f26bc9a9c28e76f5f84e4c7
yukkodev <drekgx@gmail.com>
2024-06-10 17:08:04 -0400

    six

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}<Analytics /></body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
